70a5568484dd5138c1d48758a77dfcf83efd8bd7
ravilella1234 <ravilella1234>
2019-07-31 06:07:19 +0530

    commit2

--- Practice/src/com/practice/DemoClass.java
+++ Practice/src/com/practice/DemoClass.java
@@ -2,8 +2,11 @@ package com.practice;
 
 public class DemoClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		int a=10,b=20,c=0;
+		 c=a-b;
+		 System.out.println("Addition of A  & B is : " + c);
 
 	}
 
